8cceffa0eeeffc006e79567c927f8c290c8f8230
Andebugulin <gulin.andrey2005@gmail.com>
2025-05-22 14:33:34 +0300

    exercise 4.2

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+
+const connectDB = () => {
+  const password = process.env.MONGODB_PASSWORD
+  const url = process.env.MONGODB_URI.replace('${MONGODB_PASSWORD}', encodeURIComponent(password))
+  mongoose.set('strictQuery', false)
+  mongoose.connect(url)
+    .then(() => {
+      console.log('connected to MongoDB')
+    })
+    .catch(error => {
+      console.log('error connecting to MongoDB:', error.message)
+    })
+}
+connectDB()
+
+
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
